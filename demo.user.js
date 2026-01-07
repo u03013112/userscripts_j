8ccd6f5d9c370ab250768c00e262530e4cdaeb8c
Zhijing Song <380601937@qq.com>
2026-01-07 19:15:46 +0800

    添加正确的后缀

---
+++ demo.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         测试脚本
+// @description  页面加载后弹出提示
+// @match        *://*/*
+// ==/UserScript==
+alert('Userscripts 工作正常！');
+document.body.style.border = '10px solid red';
